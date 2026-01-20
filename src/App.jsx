f4972f50f07d4b292dbf35de5e5e300c0d72e9b7
Fezwer <oleg-lib@yandex.ru>
2026-01-20 13:37:58 +0300

    fgh

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect  } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
